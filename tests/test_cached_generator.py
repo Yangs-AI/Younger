8d5eb99d5df97d5dd0a0d936a8e0dd31698f9a0c
Jason-Young-AI <AI.Jason.Young@outlook.com>
2026-02-04 10:05:41 +0800

    Start standardized testing process.

--- tests/test_cached_generator.py
+++ tests/test_cached_generator.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2026-02-04 09:19:13
+# Last Modified time: 2026-02-04 10:04:12
 # Copyright (c) 2026 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
@@ -19,8 +19,11 @@ import pathlib
 from younger.commons.cache import CachedChunks
 
 
-def test_cached_chunks():
-    itr = CachedChunks(pathlib.Path('./Test/itr'), range(100000), 1000)
+def test_cached_chunks(tmp_path: pathlib.Path):
+    itr = CachedChunks(tmp_path / "cached_chunks", range(100000), 1000)
 
+    chunks = list(itr)
     assert len(itr) == 100000
-    assert all([index == i for index, i in enumerate(itr)])
+    assert len(chunks) == 100
+    assert all(len(c) == 1000 for c in chunks[:])
+    assert sum(len(c) for c in chunks) == 100000
